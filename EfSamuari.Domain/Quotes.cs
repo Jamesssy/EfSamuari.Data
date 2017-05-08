d7cec7d071f8a264f9bcd30b2846ca9065851acc
James Johansson <shyco_2000@hotmail.com>
2017-05-08 11:41:53 +0200

    Removed usings

--- EfSamuari.Domain/Quotes.cs
+++ EfSamuari.Domain/Quotes.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-
-namespace EfSamurai.Domain
+﻿namespace EfSamurai.Domain
 {
     public class Quotes
     {
