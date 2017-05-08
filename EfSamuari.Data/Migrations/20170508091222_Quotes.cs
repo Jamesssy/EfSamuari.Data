ebc757c2a25b979f14c9f15b77b98596f3373ce1
James Johansson <shyco_2000@hotmail.com>
2017-05-08 11:18:53 +0200

    Quotes

---
+++ EfSamuari.Data/Migrations/20170508091222_Quotes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EfSamuari.Data.Migrations
+{
+    public partial class Quotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Samurais",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Quotes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SamuraiID = table.Column<int>(nullable: false),
+                    SamuraiQuotes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Quotes_Samurais_SamuraiID",
+                        column: x => x.SamuraiID,
+                        principalTable: "Samurais",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quotes_SamuraiID",
+                table: "Quotes",
+                column: "SamuraiID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotes");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Samurais");
+        }
+    }
+}
---
+++ EfSamuari.Domain/Quotes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfSamurai.Domain
+{
+    public class Quotes
+    {
+        public int Id { get; set; }
+        public string SamuraiQuotes { get; set; }
+
+        public virtual Samurai Samurai { get; set; }
+        public int SamuraiID { get; set; }
+
+
+
+
+    }
+}
